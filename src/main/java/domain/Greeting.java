50a9b7e833e2db078155d908acdaa07d851907f5
vishnu <vishnu.meppurath@gmail.com>
2019-01-19 19:40:23 +0000

    Initial commit

---
+++ src/main/java/domain/Greeting.java
@@ -0,0 +1,19 @@
+package domain;
+
+import org.springframework.stereotype.Component;
+
+public class Greeting {
+    private String greeting;
+
+    public Greeting(String greeting) {
+        this.greeting = greeting;
+    }
+
+    public String getGreeting() {
+        return greeting;
+    }
+
+    public void setGreeting(String greeting) {
+        this.greeting = greeting;
+    }
+}
---
+++ src/main/java/domain/GreetingController.java
@@ -0,0 +1,13 @@
+package domain;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+    @GetMapping("/greeting")
+    public Greeting greeting(@RequestParam(value="name",defaultValue = "World") String name){
+        return new Greeting(name);
+    }
+}
